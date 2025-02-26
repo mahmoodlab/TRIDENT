a99d48d5b91ece510f6a41b334823c1ad30a5f76
aspartate <61163004+aspartate@users.noreply.github.com>
2025-02-25 21:54:15 -0500

    always resize to square

--- trident/patch_encoder_models/utils/transform_utils.py
+++ trident/patch_encoder_models/utils/transform_utils.py
@@ -4,7 +4,7 @@ def get_eval_transforms(mean, std, target_img_size = -1, center_crop = False, **
     trsforms = []
     
     if target_img_size > 0:
-        trsforms.append(transforms.Resize(target_img_size, **resize_kwargs))
+        trsforms.append(transforms.Resize((target_img_size, target_img_size), **resize_kwargs))
     if center_crop:
         assert target_img_size > 0, "target_img_size must be set if center_crop is True"
         trsforms.append(transforms.CenterCrop(target_img_size))
