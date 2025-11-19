GJA: fix patcher with overlap + set float16 precision (#174)
* fix: patcher now overlaps also on 1st patch

* test: testing patcher behavior with overlap and masking

* doc: fix mismatch between docstring and function definition

* fix: gigapath inference on float16 instead of float32

* fix: midnight12k inference on float16 instead of float32

* fix: allow restnet50 weight to be restored from .bin or safetensors

* fix: clear heavy tissue contours when processing

trident/patch_encoder_models/load.py
@@ -575,12 +575,18 @@ class ResNet50InferenceEncoder(BasePatchEncoder):
         if weights_path:
             try:
                 model = timm.create_model("resnet50", pretrained=False, **timm_kwargs)
-                model.load_state_dict(torch.load(weights_path, map_location="cpu"), strict=False)
+                if weights_path.suffix == ".safetensors":
+                    from safetensors.torch import load_file
+                    state_dict = load_file(weights_path)
+                else:
+                    state_dict = torch.load(weights_path, map_location="cpu")
+                model.load_state_dict(state_dict, strict=False)
+
             except:
                 traceback.print_exc()
                 raise Exception(
                     f"Failed to create ResNet50 model from local checkpoint at '{weights_path}'. "
-                    "You can download the required `pytorch_model.bin` from: https://huggingface.co/timm/resnet50.tv_in1k."
+                    "You can download the required `pytorch_model.bin` or ` model.safetensors` from: https://huggingface.co/timm/resnet50.tv_in1k."
                 )
         else:
             self.ensure_has_internet(self.enc_name)
@@ -825,7 +831,7 @@ class GigaPathInferenceEncoder(BasePatchEncoder):
                 transforms.Normalize(mean, std),
             ]
         )
-        precision = torch.float32
+        precision = torch.float16
         return model, eval_transform, precision
 
     
@@ -1219,7 +1225,7 @@ class Midnight12kInferenceEncoder(BasePatchEncoder):
             ]
         )
 
-        precision = torch.float32
+        precision = torch.float16
         self.return_type = return_type
         return model, eval_transform, precision
 

trident/wsi_objects/WSIPatcher.py
@@ -106,7 +106,7 @@ class WSIPatcher:
             Custom coordinates to use. Defaults to None.
         threshold : float, optional
             Minimum proportion of the patch under tissue to be kept.
-            This argument is ignored if mask=None, passing threshold=0 will be faster. Defaults to 0.15.
+            This argument is ignored if mask=None, passing threshold=0 will be faster. Defaults to 0.0.
         pil : bool, optional
             Whether to get patches as `PIL.Image` (numpy array by default). Defaults to False.
         """
@@ -249,8 +249,8 @@ class WSIPatcher:
 
     def _colrow_to_xy(self, col, row):
         """ Convert col row of a tile to its top-left coordinates before rescaling (x, y) """
-        x = col * (self.patch_size_src) - self.overlap_src * np.clip(col - 1, 0, None)
-        y = row * (self.patch_size_src) - self.overlap_src * np.clip(row - 1, 0, None)
+        x = col * (self.patch_size_src - self.overlap_src)
+        y = row * (self.patch_size_src - self.overlap_src)
         return (x, y)   
             
     def _xy_to_colrow(self, x, y):