39913d635ff38252aaaf942a3402bade793c09cf
Guillaume Jaume <guillaume.jaume@unil.ch>
2026-04-17 18:13:18 +0200

    fix: madeleine when no path provided

--- trident/IO.py
+++ trident/IO.py
@@ -183,7 +183,7 @@ def has_internet_connection(timeout: float = 3.0) -> bool:
         return False
 
 
-def get_weights_path(model_type: str, encoder_name: str) -> str:
+def  get_weights_path(model_type: str, encoder_name: str) -> str:
     """
     Retrieve the path to the weights file for a given model name.
     This function looks up the path to the weights file in a local checkpoint
